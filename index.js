a28bf414b9291387d8800f15da6028f10390cbfe
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-01-28 21:36:30 -0500

    Add server boilerplate to index

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Server entry point
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.info("Peep port %s 🌎", process.env.PORT);
+  }
+});
