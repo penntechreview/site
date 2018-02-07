626d5f50d873b8125bc6cb001f09754f528341e6
arun <arun.kirubarajan@gmail.com>
2018-02-06 19:32:19 -0500

    Add database connection to index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-// Server entry point
 const express = require('express');
-const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+const body_parser = require('body-parser');
+const env = require('dotenv');
+const database = require('./mongoose/database');
 const app = express();
-require('dotenv').config();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+env.config();
+
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json());
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('splash');
+});
+
+app.get('/read/:id', (req, res) => {
+  res.render('article', { title: "TITLE!", body: "This is an article with body!", id: req.params.id });
+});
+
+app.get('*', (req, res) => {
+  res.render('error');
+});
 
 app.listen(process.env.PORT, err => {
   if (err) {
